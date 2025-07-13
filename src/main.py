db89c85fdbf4c1bae4fde7d387539155d8289716
andrejhulak <andrejhulak@gmail.com>
2025-07-13 11:09:37 +0200

    changed the model by making the convolutional tokenizer smaller

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ def train_model(model_config):
 if __name__ == "__main__":
   cct_configs = [
     # format: (dim, num_layers, num_heads, mlp_ratio, n_conv_layers, kernel_size, stride)
-    (128, 2, 2, 2.0, 1, 1, 1),
+    (64, 2, 2, 2.0, 1, 3, 1),
     # (128, 4, 2, 1.0, 2, 3, 1),
     # (256, 6, 4, 2.0, 2, 3, 1),
     # (256, 7, 4, 2.0, 1, 3, 1),
@@ -36,8 +36,8 @@ if __name__ == "__main__":
       'n_conv_layers': n_conv_layers,
       'kernel_size': kernel_size,
       'stride': stride,
-      'padding': 0,
-      'pooling_kernel_size': 1,
+      'padding': kernel_size // 2,
+      'pooling_kernel_size': 3,
       'pooling_stride': 1,
       'pooling_padding': 1,
       'num_layers': num_layers,
